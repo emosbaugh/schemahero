01bf8fe6816afc9af19204145732eb6dd1493631
Ethan Mosbaugh <ethan@replicated.com>
2019-08-08 16:52:41 -0700

    Set default work in progress

--- pkg/database/mysql/alter.go
+++ pkg/database/mysql/alter.go
@@ -1,29 +1,9 @@
 package mysql
 
 import (
-	"fmt"
-
 	schemasv1alpha2 "github.com/schemahero/schemahero/pkg/apis/schemas/v1alpha2"
 	"github.com/schemahero/schemahero/pkg/database/types"
 )
-
-func columnsMatch(col1 *types.Column, col2 *types.Column) bool {
-	if col1.DataType != col2.DataType {
-		return false
-	}
-
-	col1Constraints, col2Constraints := col1.Constraints, col2.Constraints
-	if col1Constraints == nil {
-		col1Constraints = &types.ColumnConstraints{}
-	}
-	if col2Constraints == nil {
-		col2Constraints = &types.ColumnConstraints{}
-	}
-
-	// TODO: default
-
-	return types.NotNullConstraintEquals(col1Constraints.NotNull, col2Constraints.NotNull)
-}
 
 func AlterColumnStatement(tableName string, primaryKeys []string, desiredColumns []*schemasv1alpha2.SQLTableColumn, existingColumn *types.Column) (string, error) {
 	// this could be an alter or a drop column command
@@ -38,30 +18,47 @@ func AlterColumnStatement(tableName string, primaryKeys []string, desiredColumns
 				return "", nil
 			}
 
-			stmt := AlterModifyColumnStatement{
-				TableName:  tableName,
-				ColumnName: column.Name,
-				DataType:   column.DataType,
-				Default:    column.ColumnDefault,
-			}
-
 			isPrimaryKey := false
 			for _, primaryKey := range primaryKeys {
 				if column.Name == primaryKey {
 					isPrimaryKey = true
 				}
 			}
-
-			if column.Constraints != nil && column.Constraints.NotNull != nil && *column.Constraints.NotNull {
-				stmt.NotNull = column.Constraints.NotNull
-			} else if !isPrimaryKey && existingColumn.Constraints != nil && existingColumn.Constraints.NotNull != nil && *existingColumn.Constraints.NotNull {
-				stmt.NotNull = new(bool)
+			// primary key cannot be null
+			if isPrimaryKey && column.Constraints != nil {
+				column.Constraints.NotNull = nil
 			}
 
-			return stmt.String(), nil
+			return AlterModifyColumnStatement{
+				TableName: tableName,
+				Column:    *column,
+			}.String(), nil
 		}
 	}
 
 	// wasn't found as a desired column, so drop
-	return fmt.Sprintf("alter table `%s` drop column `%s`", tableName, existingColumn.Name), nil
+	return AlterDropColumnStatement{
+		TableName: tableName,
+		Column:    types.Column{Name: existingColumn.Name},
+	}.String(), nil
+}
+
+func columnsMatch(col1 *types.Column, col2 *types.Column) bool {
+	if col1.DataType != col2.DataType {
+		return false
+	}
+
+	if col1.ColumnDefault != col2.ColumnDefault {
+		return false
+	}
+
+	col1Constraints, col2Constraints := col1.Constraints, col2.Constraints
+	if col1Constraints == nil {
+		col1Constraints = &types.ColumnConstraints{}
+	}
+	if col2Constraints == nil {
+		col2Constraints = &types.ColumnConstraints{}
+	}
+
+	return types.NotNullConstraintEquals(col1Constraints.NotNull, col2Constraints.NotNull)
 }
